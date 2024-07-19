1549ed51b498f1d122daae4cae670f6a738b03b4
gitirom <wadiaromdane10@gmail.com>
2024-07-19 23:18:28 +0200

    leaen the SSR SSG ISR ans static dynamic routes

---
+++ app/posts/[postId]/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            {postId}   
+        </div>
+        //this is a dynamic route 
+    )
+}
+
+export default page
---
+++ app/posts/new/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//thes is thenested route you can access it by localhost://3000/posts/new
+
+const page = () => {
+    return (
+    <div>
+      New page here! 
+    </div>
+  )
+}
+
+export default  page
