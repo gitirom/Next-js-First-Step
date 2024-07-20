8444d11f01de902e3deb55c77da7614bb4cd1b42
gitirom <wadiaromdane10@gmail.com>
2024-07-20 23:49:16 +0200

    Startting a new project !

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+import '@styles/globals.css';
+import Nav from '@componnents/Nav';
+import Provider from '@componnents/Provider'
+
+export const metadata = {                        //This metadata is useful for search engines, browsers, and social media platforms to understand the content and purpose of the web page.
+    title: 'Promptopia',
+    description: 'Discover & share AI Prompts'
+}
+
+const RootLayout = ({children}) => {
+    return (
+        <html lang='en' >                                           
+            <body>
+                <div className='main'>
+                    <div className="gradient" />
+                </div>
+
+                <main className='app' >
+                    <Nav />
+                    {children}
+                </main>
+            </body>
+        </html>
+    )
+}
+
+export default RootLayout
---
+++ componnents/Nav.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+const Nav = () => {
+    return (
+        <div>
+        Nav
+        </div>
+    )
+}
+
+export default Nav
